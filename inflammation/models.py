9cc275acb6370ae2e61fe0984cccc5ef2d7cf5a7
Laura Dias <l.monteirorentedias@erasmusmc.nl>
2025-10-31 13:34:08 +0100

    Fixed model.py

--- inflammation/models.py
+++ inflammation/models.py
@@ -9,30 +9,33 @@ and each column represents a single day across all patients.
 
 import numpy as np
 
-
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
-    :param filename: Filename of CSV to load
-    """
+    :param filename: Filename of CSV to load"""
     return np.loadtxt(fname=filename, delimiter=',')
-
 
 def daily_mean(data):
     """Calculate the daily mean of a 2d inflammation data array.
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
-    :returns: An array of mean values of measurements for each day.
-   """
-    
+
+
+    :param data: A 2D data array with inflammation data 
+    (each row contains measurements for a single patient across all days).
+    :returns: An array of mean values of measurements for each day."""
     return np.mean(data, axis=0)
 
-
 def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array."""
+    """Calculate the daily max of a 2d inflammation data array.
+    
+    :param data: A 2D data array with inflammation data 
+    (each row contains measurements for a single patient across all days).
+    :returns: An array of max values of measurements for each day."""
     return np.max(data, axis=0)
 
-
 def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array."""
+    """Calculate the daily min of a 2d inflammation data array.
+    
+    :param data: A 2D data array with inflammation data 
+    (each row contains measurements for a single patient across all days).
+    :returns: An array of minimum values of measurements for each day."""
     return np.min(data, axis=0)
-
